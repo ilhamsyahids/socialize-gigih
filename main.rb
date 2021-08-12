f7042cbef882033452c200a1be6cda76054fbeed
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-08-12 13:14:58 +0700

    add API create user

--- main.rb
+++ main.rb
@@ -10,6 +10,14 @@ before do
   content_type :json
 end
 
+before do
+  if ['PUT', 'POST', 'DELETE'].include? request.request_method
+    request.body.rewind
+    @request_payload = JSON.parse(request.body.read)
+    @request_payload.default_proc = proc{|h, k| h.key?(k.to_s) ? h[k.to_s] : nil}
+  end
+end
+
 get '/' do
   {
     message: 'Welcome Home!'
@@ -56,4 +64,36 @@ end
 get '/users' do
   users = Users.find_all
   response_generator(status, nil, Users.convert_models_to_json(users))
+end
+
+post '/users' do
+  begin
+    options = {}
+    raise "Email required" if @request_payload[:email].nil?
+    raise "Username required" if @request_payload[:username].nil?
+    id = $users_controller.create_item(@request_payload)
+    if id
+      status 201
+      message = 'User created'
+      options[:id] = id
+    else
+      raise "Bad request"
+    end
+  rescue Mysql2::Error => exception
+    if exception.message.start_with?('Duplicate entry')
+      status 409
+      if exception.message.include?('email')
+        message = 'Email already exists'
+      elsif exception.message.include?('username')
+        message = 'Username already exists'
+      else
+        message = 'User already exists'
+      end
+    end
+  rescue => exception
+    status 400
+    message = exception.message
+  ensure
+    return response_generator(status, message, options)
+  end
 end
