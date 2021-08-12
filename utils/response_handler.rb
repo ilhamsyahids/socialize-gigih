efd7254a5389861950dd09198266b61d132f92db
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-08-12 22:09:07 +0700

    change response handler structure

--- utils/response_handler.rb
+++ utils/response_handler.rb
@@ -1,8 +1,9 @@
 
 def response_generator(status, message = nil, options = {})
-  options = options.merge(:status => status)
-  if not message.nil?
-    options.merge!(:message => message)
-  end
-  options.to_json
+  response = {
+    data: options,
+    status: status
+  }
+  response[:message] = message if not message.nil?
+  response.to_json
 end
