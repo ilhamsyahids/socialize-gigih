137a998644da1cd50bc95a4457cdf46596d018b0
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-08-12 22:12:37 +0700

    add API get post by id
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- routes/posts_routes.rb
+++ routes/posts_routes.rb
@@ -19,3 +19,15 @@ get '/posts/:id' do
     .merge(Comments.convert_models_to_json(post.comments))
   return response_generator(status, message, post)
 end
+
+get '/posts/id/:id' do
+  post = $posts_controller.find_by_id(params[:id])
+  if post.nil?
+    status 404
+    message = 'Post not found'
+    return response_generator(status, message)
+  end
+  post = Posts
+    .convert_model_to_json(post)
+  response_generator(status, message, post)
+end
