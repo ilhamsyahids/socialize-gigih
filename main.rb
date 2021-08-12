3d5bf4935e81a8d100681aa76b1b6a832a1d4a4f
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-08-12 22:09:42 +0700

    add post routes

--- main.rb
+++ main.rb
@@ -1,12 +1,9 @@
 require 'sinatra'
 
 require_relative 'routes/users_routes.rb'
+require_relative 'routes/posts_routes.rb'
 
 require_relative 'utils/response_handler.rb'
-
-before do
-  content_type :json
-end
 
 before do
   if ['PUT', 'POST', 'DELETE'].include? request.request_method
@@ -14,6 +11,7 @@ before do
     @request_payload = JSON.parse(request.body.read)
     @request_payload.default_proc = proc{|h, k| h.key?(k.to_s) ? h[k.to_s] : nil}
   end
+  content_type :json
 end
 
 get '/' do
---
+++ routes/posts_routes.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+require_relative '../controllers/posts_controller.rb'
+
+require_relative '../utils/response_handler.rb'
+
+$posts_controller = PostsController.new
+
+# Posts
+get '/posts/:id' do
+  post = $posts_controller.find_with_comments_by_id(params[:id])
+  if post.nil?
+    status 404
+    message = 'Post not found'
+    return response_generator(status, message)
+  end
+  post = Posts
+    .convert_model_to_json(post)
+    .merge(Comments.convert_models_to_json(post.comments))
+  return response_generator(status, message, post)
+end
