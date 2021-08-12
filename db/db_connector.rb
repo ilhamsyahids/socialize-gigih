f3912f0b82bc5c21e4c4d06ceefffb1b8eb58243
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-08-12 10:45:39 +0700

    connect db based on env
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- db/db_connector.rb
+++ db/db_connector.rb
@@ -2,10 +2,10 @@ require 'mysql2'
 
 def create_db_client
   db = Mysql2::Client.new(
-    :host => 'localhost',
-    :username => 'root',
-    :password => 'root',
-    :database => 'socialize_db'
+    :host => ENV['DB_HOST'] || 'localhost',
+    :username => ENV['DB_USERNAME'] || 'root',
+    :password => ENV['DB_PASSWORD'] || 'root',
+    :database => ENV['DB_DATABASE'] || 'socialize_db'
   )
   db
 end
