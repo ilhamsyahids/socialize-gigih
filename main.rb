c024cbee75d514b1991e24b1a0686b3f9c3b2129
Ilham Syahid S <ilhamsyahids@gmail.com>
2021-08-13 23:21:53 +0700

    get static file API
    Signed-off-by: Ilham Syahid S <ilhamsyahids@gmail.com>

--- main.rb
+++ main.rb
@@ -16,3 +16,25 @@ get '/' do
     message: 'Welcome Home!'
   }.to_json
 end
+
+# Get static assets
+get '/assets/:type/:filename' do
+  type = params[:type]
+  filename = params[:filename]
+  if type == 'mp4'
+    content_type "video/mp4"
+  elsif ['jpg', 'jpeg'].include? type
+    content_type "image/jpeg"
+  elsif ['png', 'gif'].include? type
+    content_type "image/#{type}"
+  else
+    content_type "application/octet-stream"
+  end
+  File.read(File.join('assets', "#{type}/#{filename}"))
+end
+
+# Get all list of files belong to type
+get '/files/:type' do
+  list = Dir.glob("./assets/#{params[:type]}/*.*").map{|f| f.split('/').last}
+  response_generator(status, nil, { files: list })
+end
